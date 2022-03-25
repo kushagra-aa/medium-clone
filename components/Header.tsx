4c8505a0d086b60a196edc645d1e8479f8276554
kushagra-aa <kushagraagnihotri24@gmail.com>
2022-03-25 14:35:29 +0530

    initial commit

---
+++ components/Header.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <header>
+      <div className="flex items-center space-x-5">
+        <Link href={'/'}>
+          <img
+            className="w-44 cursor-pointer object-contain"
+            src="https://miro.medium.com/max/8978/1*s986xIGqhfsN8U--09_AdA.png"
+            alt="logo"
+          />
+        </Link>
+        <div className="hidden items-center space-x-5 md:inline-flex">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div className="">
+        <h3>Sign In</h3>
+        <h3>Get Started</h3>
+      </div>
+    </header>
+  )
+}
+
+export default Header
