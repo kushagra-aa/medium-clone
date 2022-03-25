087ff3d3d2d76ce8beabbb850276174a224c2eb0
kushagra-aa <kushagraagnihotri24@gmail.com>
2022-03-25 23:46:49 +0530

    sanity deployed and finished

---
+++ mediumclone/schemas/comment.js
@@ -0,0 +1,30 @@
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      type: 'string',
+    },
+    {
+      name: 'approved',
+      title: 'Approved',
+      type: 'boolean',
+      description: "Comments won't show on the site without aprooval",
+    },
+    {
+      name: 'email',
+      type: 'string',
+    },
+    {
+      name: 'comment',
+      type: 'text',
+    },
+    {
+      name: 'post',
+      type: 'reference',
+      to: [{ type: 'post' }],
+    },
+  ],
+}
